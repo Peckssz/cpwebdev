f46e6792f3d37f70688f13580cbf8956d5584102
Lucas Alves <109325702+heeymew@users.noreply.github.com>
2024-10-18 20:13:09 -0300

    Delete js.js

--- js.js
+++
@@ -1,3 +0,0 @@
-function mostrarAnimalInfo(nome, idade, porte, sexo, vermifugacao, vacinacao, castracao) {
-    alert(`Nome: ${nome}\nIdade: ${idade} meses\nPorte: ${porte}\nSexo: ${sexo}\nVermifugado: ${vermifugacao}\nVacinado: ${vacinacao}\nCastrado: ${castracao}`);
-}
